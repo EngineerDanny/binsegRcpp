d30ce6aac23ec7b15593707d898d6d470e1edf02
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-07-09 15:19:51 +0000

    update

---
+++ rperform/header.R
@@ -0,0 +1,13 @@
+# You can modify the PR comment header here. You can use github markdown e.g.
+# emojis like :tada:.
+
+glue::glue(
+  "The table below represents the test results generated",
+  " after running `",
+  test_function,
+  "` function",
+  " on this PR branch",
+  "\n",
+  "<br/> <br/>",
+  image_url
+)
